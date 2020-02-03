55068dcdb5f65703c2a236e3976e1162e6bb9da3
Ali Za <i3li.xz@gmail.com>
2020-02-03 21:17:05 +0300

    refactor: Breaking too long lines

--- configuration.py
+++ configuration.py
@@ -2,6 +2,7 @@
 import configparser
 from pathlib import Path
 
+
 class Configuration(object):
     """Represents a configuration.
 
@@ -10,8 +11,10 @@ class Configuration(object):
     """
 
     def __init__(self, req_path, res_path):
-        self.req_path = req_path if isinstance(req_path, Path) else Path(str(req_path))
-        self.res_path = res_path if isinstance(res_path, Path) else Path(str(res_path))
+        self.req_path = req_path if isinstance(req_path, Path) \
+            else Path(str(req_path))
+        self.res_path = res_path if isinstance(res_path, Path) \
+            else Path(str(res_path))
 
     def __repr__(self):
         return f'Request: {self.req_path} - Response: {self.res_path}'
@@ -22,11 +25,13 @@ _COMMUNICATION_SECTION = 'Communication Folders'
 _REQUEST_OPTION = 'Request'
 _RESPONSE_OPTION = 'Response'
 
+
 def get_config(config_path=_DEFAULT_CONFIG_PATH):
     """Gets the configuration
 
     Args:
-        config_path: The path of the configuration file. Defaults tp $HOME/.wakeel.ini
+        config_path: The path of the configuration file.
+                     Defaults tp $HOME/.wakeel.ini
 
     Returns:
         Configuration: The configuration object
@@ -35,14 +40,19 @@ def get_config(config_path=_DEFAULT_CONFIG_PATH):
     config = configparser.ConfigParser()
     if config.read(config_path):
         comm_section = config[_COMMUNICATION_SECTION]
-        return Configuration(comm_section[_REQUEST_OPTION], comm_section[_RESPONSE_OPTION])
+        return Configuration(
+            comm_section[_REQUEST_OPTION],
+            comm_section[_RESPONSE_OPTION]
+            )
+
 
 def set_config(new_config, config_path=_DEFAULT_CONFIG_PATH):
     """Sets a new coonfiguration
 
     Args:
         new_config (Configuration): The new configuration.
-        config_path: The path of the configuration file. Defaults tp $HOME/.wakeel.ini
+        config_path: The path of the configuration file.
+                     Defaults tp $HOME/.wakeel.ini
     """
 
     config = configparser.ConfigParser()
@@ -58,8 +68,12 @@ def set_config(new_config, config_path=_DEFAULT_CONFIG_PATH):
 
 def main():
     print(get_config())
-    # config = Configuration(str(Path.home().joinpath('req')), str(Path.home().joinpath('res')))
+    # config = Configuration(
+    #     str(Path.home().joinpath('req')),
+    #     str(Path.home().joinpath('res'))
+    # )
     # set_config(config)
+
 
 if __name__ == '__main__':
     main()
