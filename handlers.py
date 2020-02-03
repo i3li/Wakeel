67c3eba02885e91b7d53074191cc87e54889ac46
Ali Za <i3li.xz@gmail.com>
2020-02-03 22:24:49 +0300

    feat: Creating the handlers 'skeleton' structure

--- handlers.py
+++ handlers.py
@@ -9,19 +9,29 @@ class _Command(object):
 
     @staticmethod
     def parse(command_str):
-        cmd = command_str.strip(' \n').split()
+        cmd = command_str.strip(' \n').lower().split()
         return _Command(cmd[0], cmd[1:]) if len(cmd) > 0 \
             else None
 
 
+def _handle_list_files(params):
+    # TODO: Implementation
+    pass
+
+
+_HANDLERS = {
+    'list': _handle_list_files
+}
+
+
 def handle(command):
     cmd = _Command.parse(command)
-    # TODO: Handle the command
-    print(cmd)
+    if cmd and cmd.name in _HANDLERS:
+        _HANDLERS[cmd.name](cmd.params)
 
 
 # def main():
-#     handle('command p1    p2 p3  \n')
+#     handle('list p1 p2')
 #
 #
 # if __name__ == '__main__':
